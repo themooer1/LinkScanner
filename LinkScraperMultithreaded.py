2ac282d590a7a694f07bac918e00b259b4f0dc17
Eugene Wolffe <fluffy1781@gmail.com>
2016-10-14 11:23:43 -0400

    Revert "Removed Multithreading"
    This reverts commit 56fc42d098513c2a279f1abad7d544f411e0a458.

--- LinkScraperMultithreaded.py
+++ LinkScraperMultithreaded.py
@@ -1,4 +1,6 @@
 from splinter import Browser
+import threading
+import queue
 
 
 executable_path = {'executable_path':'/usr/local/bin/phantomjs'}
@@ -11,6 +13,13 @@ def loadSites():
     return siteList
 
 currenttask=0
+def scanlistThreaded(siteList, iterations=2, threads=1):
+    threads=[]
+    threadsize=int(len(siteList)/threads))
+    for i in range(0,threads):
+        t=threading.Thread(target=scanlist,args=(siteList[i*threadsize:(i+1)*threadsize],iterations=2))
+        threads.append(t)
+        t.start()
 def scanlist(siteList, iterations=2):
     global currenttask
     links=list()
@@ -30,4 +39,4 @@ def save(links,filename='links.txt'):
         linksf.write('\n'.join(map(str,links)))
         linksf.close()
 print('Hello!')
-save(scanlist(loadSites(),iterations=2))
+save(scanlist(loadSites(),iterations=2,threads=8))
