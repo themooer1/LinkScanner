921e1dd51bf9d9f648b6274ccb26ca54b39418b8
Eugene Wolffe <fluffy1781@gmail.com>
2016-10-15 15:17:14 -0400

    Bugfixes
    Patched startScan and Scan methods.
    
    Modified peekQueue class for compatibility and fixed isEmpty method.

--- LinkScraperMultithreaded.py
+++ LinkScraperMultithreaded.py
@@ -6,52 +6,51 @@ executable_path = {'executable_path':'/usr/local/bin/phantomjs'}
 b=Browser("phantomjs",**executable_path)
 class peekQueue(deque):
     def tolist(self):
-        return list(self.queue)
+        return list(self)
     def isEmpty(self):
-        return True if len(self) > 0 else False
+        return False if len(self) > 0 else True
     def peek(self,index=0):
         index=int(index)
         if not self.isEmpty():
-            return list(self)[index]
+            return self.tolist()[index]
         else:
-            raise TypeError
+            raise IndexError
 class Scanner:
     def __init__(self,siteList='siteList.txt',iterations=2,maxthreads=8):
         self.recursion=iterations
         self.linkqueue=peekQueue()
         self.maxthreads=maxthreads
         self.siteList=self.loadSites(siteList)
+        print("Sitelist:"+str(self.siteList))
         self.output=[]
+
     def loadSites(self,sitefilename):
         with open(str(sitefilename),'r') as siteListf:
             siteList = siteListf.read().split(",")
             siteListf.close()
         return siteList
     def startScan(self):
-        threads=[]
-        map(lambda x:self.linkqueue.appendleft((x,1)),self.siteList)
-        while not self.linkqueue[0][1] > self.recursion:
-            with list() as threads:
-                if len(threads) <=self.maxthreads:
-                    p=threading.Thread(target=self.scan())
-                    threads.append(p)
-                    p.start()
-                for t in threads:
-                    if not t.is_alive():
-                        threads.remove(t)
-                    print("Thread Added!")
-                else:
-                    threads[0].join()
-                    print("{} Threads Reached!".format(len(threads)))
+#        threads=[]
+        self.linkqueue.extendleft([(x,1) for x in self.siteList])
+        print(self.linkqueue.tolist())
+        while not self.linkqueue.peek(0)[1] > self.recursion:
+            if len(threading.enumerate()) <=self.maxthreads:
+                p=threading.Thread(target=self.scan)
+                p.start()
+                print("Thread Added!")
+            else:
+                threading.enumerate()[0].join()
+                print("{} Threads Reached!".format(len(threads)))
+
 
     def scan(self):
-        url = self.linkqueue.pop()
+        url,tasknum = self.linkqueue.pop()
         b.visit(str(url))
-        links = list(map(lambda x:x['href'], b.find_by_tag('a')))
+        links = [x['href'] for x in b.find_by_tag('a')]
         print("sitefinished")
         print(links)
-        map(lambda x:self.linkqueue.appendleft(x),links)
-        self.linkqueue.task_done(url)
+        for x in links:
+            self.linkqueue.extendleft((x,tasknum+1))
         self.output=self.output+links
     def save(self,filename='links.txt'):
         print("Saving...")
